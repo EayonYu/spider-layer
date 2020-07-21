bd8b36762e07d84f85dd80ed6cd8240f65e46a5b
jacob.hong <jacob.hong@tcl.com>
2020-07-21 17:15:03 +0800

    dev: support version tag

---
+++ setup.py
@@ -0,0 +1,53 @@
+import os
+
+from setuptools import setup
+
+
+# get requirements from requirements.txt
+install_requires = list()
+with open("requirements.txt", "r") as f:
+    for line in f:
+        required_package = line.rstrip()
+        if required_package:
+            install_requires.append(required_package)
+
+# get tag from env
+if os.environ.get('CI_COMMIT_TAG'):
+    version = os.environ['CI_COMMIT_TAG']
+elif os.environ.get('VERSION'):
+    version = os.environ['VERSION']
+else:
+    version = None
+
+if version:
+    setup(
+        name='spider-layer',
+        version=version,
+        description='layer for spider',
+        long_description='layer for spider',
+        long_description_content_type='text/plain',
+        author='jacob.hong',
+        author_email='jacob.hong@tcl.com',
+        license='MIT',
+        packages=['layer'],
+        url='http://10.124.106.120:18080/cloudplatform/gaia/spider/layer',
+        zip_safe=False,
+        install_requires=[
+            "easylog == 1.0.0.dev4",
+            "boto3 == 1.14.20"
+        ],
+        dependency_links=[
+            "http://10.124.106.125:9011/simple"
+        ],
+        python_requires='==3.7',
+        classifiers=[
+            'Development Status :: 5 - Production/Stable',
+            'Intended Audience :: Developers',
+            'Topic :: Software Development :: spider layer',
+            'License :: OSI Approved :: MIT License',
+            'Programming Language :: Python :: 3.7',
+        ],
+        project_urls={
+            'Source': 'http://10.124.106.120:18080/cloudplatform/gaia/spider/layer',
+        },
+    )
