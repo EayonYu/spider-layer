3cc5d9de152da36dbf26eeee2195f4e8278b533f
jacob.hong <jacob.hong@tcl.com>
2020-07-21 17:53:10 +0800

    dev

--- layer/__init__.py
+++ layer/__init__.py
@@ -1,1 +1,12 @@
 name = 'spider'
+
+
+import logging
+import sys
+logging.basicConfig(stream=sys.stdout, level=logging.DEBUG)
+
+from easylog import get_logger
+
+
+logger = get_logger()
+logger.info().msg("websocket layer")
