a1c6bf8d83872029f0231e797eafdce9167708b3
jacob.hong <jacob.hong@tcl.com>
2020-07-24 17:27:54 +0800

    dev

---
+++ layer/test/test_grpc_mirror.py
@@ -0,0 +1,17 @@
+import unittest
+
+from layer import Layer
+from layer import config
+from layer.protocol.mirror.ping_pb2 import PingRequest
+
+
+class TestGrpcMirror(unittest.TestCase):
+
+    def test_mirror(self):
+        l = Layer(config.Env.DEV)
+        mirror_client = l.grpc_mirror_client()
+        response = mirror_client.Ping(PingRequest())
+        print(response)
+
+if __name__ == '__main__':
+    unittest.main()
