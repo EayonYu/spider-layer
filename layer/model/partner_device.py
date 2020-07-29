0a2180be5cd49f8fe6e8efbc28f9ebb4810ad863
royrao <502818416@qq.com>
2020-07-29 14:31:14 +0800

    add device table

---
+++ layer/model/partner_device.py
@@ -0,0 +1,21 @@
+from ._base import BaseModel
+from ._base import CommonModel
+
+from sqlalchemy import Column, String, Text, Boolean, UniqueConstraint
+
+
+class PartnerDeviceModel(BaseModel, CommonModel):
+    __tablename__ = 'partner_device'
+
+    partner_id = Column('partner_id', String(63), nullable=False)
+    partner_device_id = Column('partner_device_id', String(63), nullable=False)
+
+    update_required = Column('update_required', Boolean, nullable=False, default=False)
+    immutable_identity = Column('immutable_identity', String(255), nullable=False)
+    device_info = Column('device_info', Text)
+    mapping_mode = Column('mapping_mode', String(63))
+
+    # TODO foreign key
+    platform_device_id = Column('platform_device_id', String(63))
+
+    __table_args__ = (UniqueConstraint('partner_id', 'partner_device_id', name='_partner_id_partner_device_id_uc'),)
